05c8507105e1d62e36894437f52b5f04bb3fb866
Carlysson.Oliveira <carlysson.oliveira@centauro.com.br>
2017-11-21 21:11:48 -0200

    Configurando FTP e Fazendo lista de imgs

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -10,12 +10,41 @@ class Upload extends CI_Controller {
 
         public function index()
         {
-                $this->load->view('upload_form', array('error' => ' ' ));
+                $this->load->library('ftp');
+                
+                $config['hostname'] = '127.0.0.1';
+                $config['username'] = 'localftp';
+                $config['password'] = '210596ca';
+                $config['port']     = 21;
+                $config['passive']  = FALSE;
+                $config['debug']    = TRUE;
+                
+                $this->ftp->connect($config);
+
+                $list = $this->ftp->list_files('.');
+                print_r($list);
+                $this->ftp->close();
+                
+                foreach($list as $img){
+                        echo '<img src='.base_url('uploads').'/'.$img.' width="500px" height="500px">';
+                }
+
+                
         }
 
         public function do_upload()
         {
-                
+               $data = array ();
+                if ($handle = opendir('./uploads')) {
+                  while (false !== ($file = readdir($handle))) {
+                         if ($file != "." && $file != "..") {
+                           $data['thelist'] .= '<li><a href="'.$file.'">'.$file.'</a></li>';
+                         }
+                      }
+                 closedir($handle);
+                 }
+               
+               
                 if ( ! $this->upload->do_upload('userfile'))
                 {
                         $error = array('error' => $this->upload->display_errors());
@@ -24,10 +53,12 @@ class Upload extends CI_Controller {
                 }
                 else
                 {
-                        $data = array('upload_data' => $this->upload->data());
 
                         $this->load->view('upload_success', $data);
                 }
+
+                
+               
         }
 }
 ?>
